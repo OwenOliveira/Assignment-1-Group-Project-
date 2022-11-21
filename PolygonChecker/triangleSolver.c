6b5e2a2e6609418c7e8cdcf4fac6dc616877718a
YoungSu Chae <Ychae1101@conestogac.on.ca>
2022-11-21 01:55:35 -0500

    Added a new function "anglesOfTriangle" that calulates the three angles of the triangle. But it's not working properly yet.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -6,19 +6,34 @@
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
-		result = "Not a triangle";
+		result = "Not a triangle\n";
 	}
 	else if (side1 == side2 && side1 == side3) {
-		result = "Equilateral triangle";
+		result = "Equilateral triangle\n";
 	}
 	else if ((side1 == side2 && side1 != side3) || 
 		(side1 == side3 && side1 != side2))
 	{
-		result = "Isosceles triangle";
+		result = "Isosceles triangle\n";
 	}
 	else {
-		result = "Scalene triangle";
+		result = "Scalene triangle\n";
 	}
 
 	return result;
+}
+
+void anglesOfTriangle(int a, int b, int c)
+{
+	double angleA, angleB, angleC, s;
+	const double PI = 3.1415926;
+
+	s = ((b * b) + (c * c) - (a * a)) / (2 * b * c);
+	angleA = acos(s) * 180 / PI;
+	angleB = acos(((a * a) + (c * c) - (b * b)) / (2 * a * c)) * (180 / PI);
+	angleC = acos(((b * b) + (a * a) - (c * c)) / (2 * b * a)) * 180 / PI;
+
+	printf("The angle between %d and %d is %g dgree.\n", b, c, angleA);
+	printf("The angle between %d and %d is %g dgree.\n", a, c, angleB);
+	printf("The angle between %d and %d is %g dgree.\n", b, a, angleC);
 }
--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,2 +1,7 @@
-#pragma once
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+#define TOTALANGLES 180
 char* analyzeTriangle(int side1, int side2, int side3);
+
+void anglesOfTriangle(int a, int b, int c);
