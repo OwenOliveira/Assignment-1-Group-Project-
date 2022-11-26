384dbce93c9a05f867a6909d49a72151d8237d51
Saba <sberenji4567@conestogac.on.ca>
2022-11-26 16:53:43 -0500

    Fixed the problem in the triangle type test cases and added two test cases for the isRectangle function

--- Test_Rectangle/TestCases.cpp
+++ Test_Rectangle/TestCases.cpp
@@ -1,7 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
-extern "C" void isRectangle(POINTS pointarr[]);
+extern "C" char* isRectangle(double distA, double distB, double distC, double distD, double distE, double distF);
+extern "C" double rectangleDist(POINTS p, POINTS q);
 extern "C" double perimeterRectangle(double, double, double, double);
 extern "C" double CalculateArea(double dista, double distb);
 extern "C" char* analyzeTriangle(double a, double b, double c);
@@ -11,34 +12,45 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace TestCases
 {
-	TEST_CLASS(TestCases)
+	TEST_CLASS(RectangleTestCases)
 	{
 	public:
 		
-		TEST_METHOD(FourPointsFunctionality)
+		TEST_METHOD(Test1_isRectangle_Functionality)  
 		{
+			//Testing the isRectangle function, whether the points form a rectangle or not (should form a rectangle in this case)
+
 			POINTS points[SIZE] = { {5,2}, {-5,2}, {-5,-2}, {5,-2} };
-			//This is testing the rectangle feature, whether the points form a rectangle or not (should form a rectangle in this case)
-			/*char* result = isRectangle(points);*/
-			/*Assert::AreEqual("Is rectangle", result);*/
+
+			double dist12 = rectangleDist(points[0], points[1]);
+			double dist23 = rectangleDist(points[1], points[2]);
+			double dist34 = rectangleDist(points[2], points[3]);
+			double dist14 = rectangleDist(points[0], points[3]);
+			double dist24 = rectangleDist(points[1], points[3]);
+			double dist13 = rectangleDist(points[0], points[2]);
+
+			char* result = isRectangle(dist12, dist34, dist14, dist23, dist24, dist13);
+			Assert::AreEqual("The points make a rectangle", result);
 		}
 		
-		TEST_METHOD(FourPointsFunctionality)
+		TEST_METHOD(Test2_isRectangle_Functionality)
 		{
-			POINTS points[SIZE] = { {5,6}, {-5,2}, {-5,-4}, {5,-2} };
-			//This is testing the rectangle feature, whether the points form a rectangle or not (should not form a rectangle in this case)
-			/*char* result = isRectangle(points);
-			char* result = isRectangle(points);*/
-			/*Assert::AreEqual("Is not a rectangle", result);*/
+			//This is testing the isRectangle function, whether the points form a rectangle or not (should NOT form a rectangle in this case)
+			
+			POINTS points[SIZE] = { {6,2}, {-5,2}, {-5,-2}, {5,-2} };
+
+			double dist12 = rectangleDist(points[0], points[1]);
+			double dist23 = rectangleDist(points[1], points[2]);
+			double dist34 = rectangleDist(points[2], points[3]);
+			double dist14 = rectangleDist(points[0], points[3]);
+			double dist24 = rectangleDist(points[1], points[3]);
+			double dist13 = rectangleDist(points[0], points[2]);
+
+			char* result = isRectangle(dist12, dist34, dist14, dist23, dist24, dist13);
+			Assert::AreEqual("The points do NOT make a rectangle", result);
 		}
-		
-		/*TEST_METHOD(TestMethod3)
-		{
-		}
-		
-		TEST_METHOD(TestMethod4)
-		{
-		}*/
+
+
 	};
 }
 
@@ -46,112 +58,17 @@ namespace TestCases
 
 //  triangle test cases - please note the AnglesOfTriangle function appears to need correction. 
 
-namespace TriangleSolver
+
+namespace TestCases
 {
-	TEST_CLASS(TriangleSolver) // testing analyzeTriangle
+	TEST_CLASS(TriangleTypeTestCases) // testing analyzeTriangle
 	{
 	public:
-		
-		TEST_METHOD(Test1_analyzeTriangle_Equilateral_true) 
+		TEST_METHOD(Test1_analyzeTriangle_Functionality) // testing for Equilateral triangle
 		{
-			int side1;
-			int side2;
-			int side3;
-			char* result;
-
-			side1 = 4;
-			side2 = 4;
-			side3 = 4;
-
-			result = analyzeTriangle(side1, side2, side3);
-
-			Assert::AreEqual("Equilateral triangle", result);
-		}
-
-		TEST_METHOD(Test2_analyzeTriangle_Equilateral_false)
-		{
-			int side1;
-			int side2;
-			int side3;
-			char* result;
-
-			side1 = 4;
-			side2 = 7;
-			side3 = 4;
-
-			result = analyzeTriangle(side1, side2, side3);
-
-			Assert::AreNotEqual("Equilateral triangle", result);
-		}
-
-		TEST_METHOD(Test3_analyzeTriangle_Isosoles_true)
-		{
-			int side1;
-			int side2;
-			int side3;
-			char* result;
-
-			side1 = 5;
-			side2 = 3;
-			side3 = 5;
-
-			result = analyzeTriangle(side1, side2, side3);
-
-			Assert::AreEqual("Isosoles triangle", result);
-		}
-
-		TEST_METHOD(Test4_analyzeTriangle_Isosoles_false)
-		{
-			int side1;
-			int side2;
-			int side3;
-			char* result;
-
-			side1 = 4;
-			side2 = 4;
-			side3 = 4;
-
-			result = analyzeTriangle(side1, side2, side3);
-
-			Assert::AreNotEqual("Isosoles triangle", result);
-		}
-
-
-		TEST_METHOD(Test5_analyzeTriangle_Scalene_true)
-		{
-			int side1;
-			int side2;
-			int side3;
-			char* result;
-
-			side1 = 3;
-			side2 = 4;
-			side3 = 5;
-
-			result = analyzeTriangle(side1, side2, side3);
-
-			Assert::AreEqual("Scalene triangle", result);
-		}
-
-		TEST_METHOD(Test6_analyzeTriangle_Scalene_false)
-		{
-			int side1;
-			int side2;
-			int side3;
-			char* result;
-
-			side1 = 4;
-			side2 = 4;
-			side3 = 9;
-
-			result = analyzeTriangle(side1, side2, side3);
-
-			Assert::AreNotEqual("Scalene triangle", result);
-		}
-
-
-		TEST_METHOD(Test7_analyzeTriangle_notTriangle_true)
-		{
+			/*This is testing the analyzeTriangle function, whether the given sides form a triangle or not, 
+			if yes what kind of triangle (should NOT form a triangle in this case)*/
+			
 			int side1;
 			int side2;
 			int side3;
@@ -163,11 +80,14 @@ namespace TriangleSolver
 
 			result = analyzeTriangle(side1, side2, side3);
 
-			Assert::AreEqual("Not A Triangle", result);
+			Assert::AreEqual("Not a triangle\n", result);
 		}
 
-		TEST_METHOD(Test8_analyzeTriangle_notTriangle_false) // is a triangle but false for this. 
+		TEST_METHOD(Test2_analyzeTriangle_Functionality) 
 		{
+			/*This is testing the analyzeTriangle function, whether the given sides form a triangle or not,
+			if yes what kind of triangle (should form a triangle in this case)*/
+
 			int side1;
 			int side2;
 			int side3;
@@ -179,47 +99,180 @@ namespace TriangleSolver
 
 			result = analyzeTriangle(side1, side2, side3);
 
-			Assert::AreNotEqual("Not A Triangle", result);
+			Assert::AreNotEqual("Not a triangle\n", result);
 		}
-
-	};
-
-//
-namespace AnglesOfTriangle
-{
-	TEST_CLASS(AnglesOfTriangle) // testing angles of triange for 180 (equilateral sides)
-	{
-	public:
- 
-		TEST_METHOD(Test1_AnglesOfTrianglesTest_Equilateral_true)
+		TEST_METHOD(Test3_analyzeTriangle_Functionality)
 		{
- 
-			double side1 = 6;  // i feel like these should be float because it is math
-			double side2 = 6;
-			double side3 = 6;
+			/*This is testing the analyzeTriangle function, whether the given sides form a triangle or not,
+			if yes what kind of triangle (should form an equilateral triangle in this case)*/
 
-			/*float result = */anglesOfTriangle(side1, side2, side3);
+			int side1;
+			int side2;
+			int side3;
+			char* result;
 
-			/*int sum = 180.f;
- 
-			Assert::AllAreEqual(sum, result);*/
+			side1 = 4;
+			side2 = 4;
+			side3 = 4;
+
+			result = analyzeTriangle(side1, side2, side3);
+
+			Assert::AreEqual("Equilateral triangle\n", result);
+		}
+
+		TEST_METHOD(Test4_analyzeTriangle_Equilateral_Functionality)
+		{
+			/*This is testing the analyzeTriangle function, whether the given sides form a triangle or not,
+			if yes what kind of triangle (should NOT form an equilateral triangle in this case)*/
+
+			int side1;
+			int side2;
+			int side3;
+			char* result;
+
+			side1 = 4;
+			side2 = 7;
+			side3 = 4;
+
+			result = analyzeTriangle(side1, side2, side3);
+
+			Assert::AreNotEqual("Equilateral triangle\n", result);
+		}
+
+		TEST_METHOD(Test5_analyzeTriangle_Isosoles_Functionality)
+		{
+			/*This is testing the analyzeTriangle function, whether the given sides form a triangle or not,
+			if yes what kind of triangle (should form an isosceles triangle in this case)*/
+
+			int side1;
+			int side2;
+			int side3;
+			char* result;
+
+			side1 = 5;
+			side2 = 5;
+			side3 = 3;
+
+			result = analyzeTriangle(side1, side2, side3);
+
+			Assert::AreEqual("Isosceles triangle\n", result);
+		}
+
+		TEST_METHOD(Test6_analyzeTriangle_Isosoles_Functionality_True)
+		{
+			/*This is testing the analyzeTriangle function, whether the given sides form a triangle or not,
+		    if yes what kind of triangle (should form an isosceles triangle in this case)*/
+
+			int side1;
+			int side2;
+			int side3;
+			char* result;
+
+			side1 = 5;
+			side2 = 8;
+			side3 = 5;
+
+			result = analyzeTriangle(side1, side2, side3);
+
+			Assert::AreEqual("Isosceles triangle\n", result);
+		}
+
+		TEST_METHOD(Test7_analyzeTriangle_Isosoles_Functionality)
+		{
+			/*This is testing the analyzeTriangle function, whether the given sides form a triangle or not,
+		    if yes what kind of triangle (should NOT form an isosceles triangle in this case)*/
+
+			int side1;
+			int side2;
+			int side3;
+			char* result;
+
+			side1 = 5;
+			side2 = 8;
+			side3 = 7;
+
+			result = analyzeTriangle(side1, side2, side3);
+
+			Assert::AreNotEqual("Isosceles triangle\n", result);
 		}
 
 
-		//TEST_METHOD(Test2_AnglesOfTrianglesTest_ _false)  // next test here 
-		//{
+		TEST_METHOD(Test8_analyzeTriangle_Scalene_Functionality)
+		{
+			/*This is testing the analyzeTriangle function, whether the given sides form a triangle or not,
+		    if yes what kind of triangle (should form a scalene triangle in this case)*/
 
-		//	float side1 = ;  // i feel like these should be float because it is math
-		//	float side2 = ;
-		//	float side3 = ;
+			int side1;
+			int side2;
+			int side3;
+			char* result;
 
-		//	float result = getAnglesOfTriangle(side1, side2, side3);
+			side1 = 4;
+			side2 = 5;
+			side3 = 6;
 
-		//	float sum = 180.f;
+			result = analyzeTriangle(side1, side2, side3);
 
-		//	Assert::AllAreEqual(sum, result);
-		//}
-	}
+			Assert::AreEqual("Scalene triangle\n", result);
+		}
 
+		TEST_METHOD(Test9_analyzeTriangle_Scalene_Functionality_False)
+		{
+			/*This is testing the analyzeTriangle function, whether the given sides form a triangle or not,
+			if yes what kind of triangle (should NOT form a scalene triangle in this case)*/
 
+			int side1;
+			int side2;
+			int side3;
+			char* result;
+
+			side1 = 4;
+			side2 = 4;
+			side3 = 9;
+
+			result = analyzeTriangle(side1, side2, side3);
+
+			Assert::AreNotEqual("Scalene triangle\n", result);
+		}
+	};
 }
+
+////
+//namespace AnglesOfTriangle
+//{
+//	TEST_CLASS(AnglesOfTriangle) // testing angles of triange for 180 (equilateral sides)
+//	{
+//	public:
+// 
+//		TEST_METHOD(Test1_AnglesOfTrianglesTest_Equilateral_true)
+//		{
+// 
+//			double side1 = 6;  // i feel like these should be float because it is math
+//			double side2 = 6;
+//			double side3 = 6;
+//
+//			/*float result = */anglesOfTriangle(side1, side2, side3);
+//
+//			/*int sum = 180.f;
+// 
+//			Assert::AllAreEqual(sum, result);*/
+//		}
+//
+//
+//		//TEST_METHOD(Test2_AnglesOfTrianglesTest_ _false)  // next test here 
+//		//{
+//
+//		//	float side1 = ;  // i feel like these should be float because it is math
+//		//	float side2 = ;
+//		//	float side3 = ;
+//
+//		//	float result = getAnglesOfTriangle(side1, side2, side3);
+//
+//		//	float sum = 180.f;
+//
+//		//	Assert::AllAreEqual(sum, result);
+//		//}
+//	}
+//
+//
+//}
